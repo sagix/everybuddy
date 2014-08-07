2c1b561a6944e3d43431a89ecfa10145ffc3e785
mouchel <mouchel@lemonde.fr>
2014-08-07 14:05:01 +0200

    first commit
    Update api.json

---
+++ index.js
@@ -0,0 +1,32 @@
+var http = require('http'),
+    url = require('url'),
+    path = require('path'),
+    fs = require('fs');
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "css": "text/css",
+    "js": "text/javascript",
+    "json": "tapplication/json",
+    "css": "text/css"};
+
+http.createServer(function(req, res) {
+    var uri = url.parse(req.url).pathname;
+    var filename = path.join(process.cwd(), uri);
+    fs.exists(filename, function(exists) {
+        if(!exists) {
+            console.log("not exists: " + filename);
+            res.writeHead(404, {'Content-Type': 'text/plain'});
+            res.write('404 Not Found\n');
+            res.end();
+        }
+        var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+        res.writeHead(200, mimeType);
+
+        var fileStream = fs.createReadStream(filename);
+        fileStream.pipe(res);
+
+    }); //end path.exists
+}).listen(1337);
